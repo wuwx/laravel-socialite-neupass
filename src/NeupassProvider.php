7a155a82bbed6ce05463c4fa9012bdaee88ff8fb
wuwx <wuweixin@gmail.com>
2020-03-06 21:01:48 +0800

    重构实现

---
+++ src/NeupassProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wuwx\LaravelSocialiteNeupass;
+
+use Illuminate\Support\Facades\Session;
+use Laravel\Socialite\Contracts\Provider as ProviderContract;
+use phpCAS;
+
+class NeupassProvider implements ProviderContract
+{
+    public function __construct()
+    {
+        session_set_save_handler(Session::getHandler());
+
+        phpCAS::client(CAS_VERSION_2_0, "pass.neu.edu.cn", 443, "tpass");
+
+        phpCAS::setNoCasServerValidation();
+    }
+
+    public function redirectUrl($url)
+    {
+        phpCAS::setFixedServiceURL($url);
+        return $this;
+    }
+
+    public function redirect()
+    {
+        return redirect(phpCAS::getServerLoginURL());
+    }
+
+    public function user()
+    {
+        phpCAS::forceAuthentication();
+        return (new User)->map([
+            'id' => phpCAS::getUser(),
+            'name' => phpCAS::getAttribute('USER_NAME'),
+        ]);
+    }
+}
--- src/NeupassServiceProvider.php
+++ src/NeupassServiceProvider.php
@@ -10,26 +10,7 @@ class NeupassServiceProvider extends ServiceProvider
     public function boot()
     {
         Socialite::extend('neupass', function() {
-            return new class {
-                public function __construct()
-                {
-                    phpCAS::client(CAS_VERSION_2_0, "pass.neu.edu.cn", 443, "tpass");
-                    phpCAS::setNoCasServerValidation();
-                }
-                public function redirect()
-                {
-                    phpCAS::setFixedServiceURL(phpCAS::getServiceURL() . "/callback");
-                    return redirect(phpCAS::getServerLoginURL());
-                }
-                public function user()
-                {
-                    phpCAS::forceAuthentication();
-                    return (new User)->map([
-                        'id' => phpCAS::getUser(),
-                        'name' => phpCAS::getAttribute('USER_NAME'),
-                    ]);
-                }
-            };
+            return new NeupassProvider();
         });
     }
 
